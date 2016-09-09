17bd7d435b05ba311966e08efa43afa55ac5ec84
gulfer <kurt_77@126.com>
2016-09-09 14:51:42 +0800

    update

--- src/com/gulferorkurt/java8/lambda/LambdaTest.java
+++ src/com/gulferorkurt/java8/lambda/LambdaTest.java
@@ -5,5 +5,20 @@ public class LambdaTest {
 	public static void main(String[] args) {
 		
 	}
+	
+	static void startThreadWithLambda() {
+		new Thread(() -> System.out.println("start thread with lambda")).start();
+	}
+	
+	static void startThreadWithInner() {
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("start thread with inner class");
+			}
+			
+		}).start();
+	}
 
 }
